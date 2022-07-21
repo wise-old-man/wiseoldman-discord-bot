2a430c868e01deb4513d79fdfcc774d9442e9039
Robin <rorrosliwa@gmail.com>
2022-07-21 22:14:08 +0200

    Add sharding

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+import { Shard, ShardingManager } from 'discord.js';
+import config from './config';
 import * as api from './api';
-import bot from './bot';
+import { join } from 'path';
 
-bot.init();
+const manager = new ShardingManager(join(__dirname, 'base'), { token: config.token });
+
+manager.on('shardCreate', (shard: Shard) => console.log(`[SHARD] Launched  shard ${shard.id}`));
+
+manager.spawn();
+
 api.init();
