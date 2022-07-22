eac2d075570cb73bbcef98e95cd21b2703f22401
Psikoi <ruben.amendoeira@gmail.com>
2022-07-22 12:25:43 +0100

    Revert back to dist/base.js

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { Shard, ShardingManager } from 'discord.js';
 import config from './config';
 import * as api from './api';
-import { join } from 'path';
 
-const manager = new ShardingManager(join(__dirname, 'base'), { token: config.token });
+const manager = new ShardingManager('dist/base.js', { token: config.token });
 
 manager.on('shardCreate', (shard: Shard) => console.log(`[SHARD] Launched  shard ${shard.id}`));
 
