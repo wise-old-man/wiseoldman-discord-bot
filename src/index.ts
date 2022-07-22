3a1eff904e8306940f58da2d1a0fcb685b97ed57
Robin <5983417+rorro@users.noreply.github.com>
2022-07-22 13:26:36 +0200

    Add sharding (#137)
    * Add sharding
    
    * Revert back to dist/base.js
    
    Co-authored-by: Psikoi <ruben.amendoeira@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
+import { Shard, ShardingManager } from 'discord.js';
+import config from './config';
 import * as api from './api';
-import bot from './bot';
 
-bot.init();
+const manager = new ShardingManager('dist/base.js', { token: config.token });
+
+manager.on('shardCreate', (shard: Shard) => console.log(`[SHARD] Launched  shard ${shard.id}`));
+
+manager.spawn();
+
 api.init();
