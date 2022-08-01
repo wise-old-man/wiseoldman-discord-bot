d817ba47ced41eef010a925e89bd4e51f45d16c0
Robin <5983417+rorro@users.noreply.github.com>
2022-08-01 15:01:28 +0200

    Revert "Add sharding (#137)"
    This reverts commit 3a1eff904e8306940f58da2d1a0fcb685b97ed57.

--- src/base.ts
+++
@@ -1,3 +0,0 @@
-import bot from './bot';
-
-bot.init();
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
-import { Shard, ShardingManager } from 'discord.js';
-import config from './config';
 import * as api from './api';
+import bot from './bot';
 
-const manager = new ShardingManager('dist/base.js', { token: config.token });
-
-manager.on('shardCreate', (shard: Shard) => console.log(`[SHARD] Launched  shard ${shard.id}`));
-
-manager.spawn();
-
+bot.init();
 api.init();
