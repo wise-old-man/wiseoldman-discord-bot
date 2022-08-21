4a29c7d0773f68980a91e7d4f2ca53780add9cd5
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2022-08-21 21:32:10 +0100

    Change button text

--- src/commands/parser.ts
+++ src/commands/parser.ts
@@ -46,7 +46,7 @@ From *September 1st* and forward this bot will **only** support slash commands.
 
 You might need to update the bot's permissions. The easiest way to do this is to kick and re-invite the bot. You can do that by either:
 
-- Clicking the bot's name on Discord's right-side panel, and then clicking the "Invite" button.
+- Clicking the bot's name on Discord's right-side panel, and then clicking the "Add to Server" button.
 - Going to https://bot.wiseoldman.net/ and clicking the "Add to Discord" button.
 
 You can read more about this decision at <https://bit.ly/3AD4zsM>`;
