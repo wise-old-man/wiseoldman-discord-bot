2b4cba70965eeafe5c6f87afcccd9ccb4dc6decf
Psikoi <ruben.amendoeira@gmail.com>
2023-02-11 13:43:57 +0100

    Update sentry env

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import bot from './bot';
 import { deployCommands } from './deploy-commands';
 
 Sentry.init({
-  dsn: process.env.DISCORD_BOT_SENTRY_DSN,
+  dsn: process.env.BOT_SENTRY_DSN,
   tracesSampleRate: 0.01
 });
 
