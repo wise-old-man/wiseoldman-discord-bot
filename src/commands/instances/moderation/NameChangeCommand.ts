838be88ce4d096637fb8715ac27389d165000b18
Psikoi <ruben.amendoeira@gmail.com>
2023-02-25 14:16:10 +0100

    Handle 500 and 504 errors in name change review

--- src/commands/instances/moderation/NameChangeCommand.ts
+++ src/commands/instances/moderation/NameChangeCommand.ts
@@ -35,6 +35,8 @@ class NameChangeCommand extends Command {
 
     const reviewData = await womClient.nameChanges.getNameChangeDetails(nameChangeId).catch(e => {
       if (e.statusCode === 404) throw new CommandError('Name change ID not found.');
+      if (e.statusCode === 500) throw new CommandError('Failed to load hiscores. Please try again.');
+
       throw e;
     });
 
@@ -89,12 +91,19 @@ class NameChangeCommand extends Command {
 
     collector?.on('end', async collection => {
       const buttonClicked = collection.first()?.customId;
+
       if (buttonClicked === `namechange_approve/${nameChangeId}`) {
         try {
           await approveNameChange(nameChangeId);
           response.setFooter({ text: `Approved ✅` }).setColor(config.visuals.green);
         } catch (error) {
-          response.setFooter({ text: 'Failed to approve name change' }).setColor(config.visuals.red);
+          if ('statusCode' in error && error.statusCode === 504) {
+            response
+              .setFooter({ text: 'Approval timed out. Check the status again in a few minutes.' })
+              .setColor(config.visuals.orange);
+          } else {
+            response.setFooter({ text: 'Failed to approve name change' }).setColor(config.visuals.red);
+          }
         }
       } else if (buttonClicked === `namechange_deny/${nameChangeId}`) {
         try {
