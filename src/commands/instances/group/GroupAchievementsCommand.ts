4ec52e0418cbdeef912025aeca5b85a35ddb5a25
Alec Caputo <acaputo@mail.smcvt.edu>
2023-03-10 08:51:33 -0700

    #170 add group achievements command
    #170 add group achievements command. Will return the most recent player achievements per the configured group

---
+++ src/commands/instances/group/GroupAchievementsCommand.ts
@@ -0,0 +1,57 @@
+import { bold, Command, CommandConfig, CommandError, getEmoji, getLinkedGroupId } from '../../../utils';
+import {
+  formatNumber,
+  getMetricName,
+  isMetric,
+  isPeriod,
+  Metric,
+  parseMetricAbbreviation,
+  Period
+} from '@wise-old-man/utils';
+import { CommandInteraction, MessageEmbed } from 'discord.js';
+import womClient from '../../../services/wiseoldman';
+import config from '../../../config';
+
+const CONFIG: CommandConfig = {
+  name: 'achievements',
+  description: "View a group's most recent achievements."
+};
+
+class GroupAchievements extends Command {
+  constructor() {
+    super(CONFIG);
+  }
+
+  async execute(interaction: CommandInteraction) {
+    const groupId = await getLinkedGroupId(interaction);
+
+    try {
+      const groupAchievements = await womClient.groups.getGroupAchievements(groupId);
+      const players = await Promise.all(
+        groupAchievements.map(x => womClient.players.getPlayerDetailsById(x.playerId))
+      );
+
+      const achievementList = groupAchievements
+        .map(
+          (ach, idx) =>
+            `${idx + 1}. ${bold(players.find(x => x.id === ach.playerId).displayName)} - ${getEmoji(
+              ach.metric
+            )} ${ach.name}`
+        )
+        .join('\n');
+
+      const response = new MessageEmbed()
+        .setColor(config.visuals.blue)
+        .setTitle('Recent Group Achievements')
+        .setDescription(achievementList)
+        .setURL(`https://wiseoldman.net/groups/${groupId}/achievements/`)
+        .setFooter({ text: `Tip: Try /group records metric: zulrah period: day` });
+
+      await interaction.editReply({ embeds: [response] });
+    } catch (e) {
+      throw new CommandError("Couldn't find that group or player.");
+    }
+  }
+}
+
+export default new GroupAchievements();
