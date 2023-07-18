64440e99a21a7e38e1dc3bcd15757d0ca6cb8f65
Psikoi <ruben.amendoeira@gmail.com>
2023-07-19 01:00:54 +0200

    Add "create api key" mod command

---
+++ src/commands/instances/moderation/CreateAPIKeyCommand.ts
@@ -0,0 +1,60 @@
+import { CommandInteraction, MessageEmbed } from 'discord.js';
+import { createAPIKey } from '../../../services/wiseoldman';
+import config from '../../../config';
+import { Command, CommandConfig, sendModLog } from '../../../utils';
+
+const CONFIG: CommandConfig = {
+  name: 'create-api-key',
+  description: 'Create a new Wise Old Man API key.',
+  options: [
+    {
+      type: 'string',
+      required: true,
+      name: 'project',
+      description: 'The name of the project this key is meant to be used by.'
+    },
+    {
+      type: 'user',
+      required: true,
+      name: 'requester',
+      description: "Requester's Discord user tag."
+    }
+  ]
+};
+
+class CreateAPIKeyCommand extends Command {
+  constructor() {
+    super(CONFIG);
+    this.private = true;
+    this.moderation = true;
+  }
+
+  async execute(interaction: CommandInteraction) {
+    const project = interaction.options.getString('project', true);
+    const requesterId = interaction.options.getUser('requester', false)?.id;
+
+    const requester = interaction.guild?.members.cache.find(m => m.id === requesterId);
+
+    if (!requester) throw new Error();
+
+    const key = await createAPIKey(project, requester.user.username);
+
+    // Respond on the WOM discord chat with a success status
+    const response = new MessageEmbed()
+      .setColor(config.visuals.green)
+      .setDescription(
+        `✅ API key created for "${project}":\n\`${key.id}\`\n\n<https://docs.wiseoldman.net/#rate-limits--api-keys>`
+      );
+
+    await interaction.editReply({ embeds: [response] });
+
+    sendModLog(
+      interaction.guild,
+      `API Key created (Project: ${project})`,
+      interaction.user,
+      requester?.user
+    );
+  }
+}
+
+export default new CreateAPIKeyCommand();
