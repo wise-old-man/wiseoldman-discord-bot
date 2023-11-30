66389a32b828d7f7128ea48c0d9b3a05e3dd2058
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2023-11-30 19:13:14 +0100

    Add claim ªªleagueªª patreon benefits button (#228)
    * Add claim league benefits button
    
    * Remove logs

--- src/patreon-trigger.ts
+++ src/patreon-trigger.ts
@@ -12,10 +12,13 @@ import {
 } from 'discord.js';
 import config from './config';
 import { hasRole, sendModLog } from './utils';
-import { claimBenefits } from './services/wiseoldman';
+import { claimBenefits, claimLeagueBenefits } from './services/wiseoldman';
 
 export const PATREON_MODAL_ID = 'patreon-benefits-modal';
+export const PATREON_LEAGUE_MODAL_ID = 'patreon-league-benefits-modal';
+
 export const PATREON_TRIGGER_ID = 'patreon-benefits-trigger';
+export const PATREON_LEAGUE_TRIGGER_ID = 'patreon-league-benefits-trigger';
 
 const NOT_A_PATRON_ERROR_MESSAGE = `Only Patreon supporters can claim benefits, please consider helping fund the project at https://wiseoldman.net/patreon.\n\nIf you already are a Patreon supporter, make sure to connect your Discord account to your Patreon account.`;
 
@@ -37,7 +40,11 @@ export async function setupPatreonTrigger(client: Client) {
     new MessageButton()
       .setCustomId(PATREON_TRIGGER_ID)
       .setLabel('Claim Patreon Benefits')
-      .setStyle('SUCCESS')
+      .setStyle('SUCCESS'),
+    new MessageButton()
+      .setCustomId(PATREON_LEAGUE_TRIGGER_ID)
+      .setLabel('(🏆 Leagues) Claim Patreon Benefits')
+      .setStyle('SECONDARY')
   );
 
   const message = await patreonInfoChannel.send({ content, components: [actions] });
@@ -71,6 +78,47 @@ export async function handlePatreonTrigger(interaction: ButtonInteraction) {
   const groupIdInput = new TextInputComponent()
     .setCustomId('groupId')
     .setLabel("Your group's ID")
+    .setPlaceholder("Ex: 139 (Can be found on your group's page URL.)")
+    .setStyle(1);
+
+  // @ts-expect-error -- Typings are wrong on discord.js v13.7.0 (can be deleted on a v14 upgrade)
+  modal.addComponents(new MessageActionRow().addComponents(usernameInput));
+
+  if (isTier2Supporter) {
+    // @ts-expect-error -- Typings are wrong on discord.js v13.7.0 (can be deleted on a v14 upgrade)
+    modal.addComponents(new MessageActionRow().addComponents(groupIdInput));
+  }
+
+  interaction.showModal(modal);
+}
+
+export async function handlePatreonLeagueTrigger(interaction: ButtonInteraction) {
+  if (!interaction.member) return;
+
+  const member = interaction.member as GuildMember;
+
+  if (!hasRole(member, config.discord.roles.patreonSupporter)) {
+    await interaction.reply({ content: NOT_A_PATRON_ERROR_MESSAGE, ephemeral: true });
+    return;
+  }
+
+  const isTier2Supporter = hasRole(member, config.discord.roles.patreonSupporterT2);
+
+  const modal = new Modal()
+    .setCustomId(PATREON_LEAGUE_MODAL_ID)
+    .setTitle(`Claim League Benefits (Tier ${isTier2Supporter ? 2 : 1})`);
+
+  const usernameInput = new TextInputComponent()
+    .setCustomId('username')
+    .setLabel('Your in-game username')
+    .setPlaceholder('Ex: Zezima')
+    .setMaxLength(12)
+    .setStyle(1)
+    .setRequired(true);
+
+  const groupIdInput = new TextInputComponent()
+    .setCustomId('groupId')
+    .setLabel("Your League group's ID")
     .setPlaceholder("Ex: 139 (Can be found on your group's page URL.)")
     .setStyle(1);
 
@@ -125,7 +173,50 @@ export async function handlePatreonModalSubmit(interaction: ModalSubmitInteracti
       } Patreon benefits for: \nUsername: ${username}${groupId ? `\nGroup id: ${groupId}` : ''}`
     );
   } catch (error) {
-    console.log(error);
+    interaction.reply({ content: error.message, ephemeral: true });
+  }
+}
+
+export async function handlePatreonLeagueModalSubmit(interaction: ModalSubmitInteraction) {
+  const username = interaction.fields.getTextInputValue('username');
+
+  if (!username) {
+    interaction.reply({ content: '❌ Please provide your in-game username.', ephemeral: true });
+    return;
+  }
+
+  let groupId: number | undefined;
+
+  if (hasRole(interaction.member as GuildMember, config.discord.roles.patreonSupporterT2)) {
+    const groupIdValue = interaction.fields.getTextInputValue('groupId');
+    const isInteger = typeof groupIdValue === 'string' && Number.isInteger(parseInt(groupIdValue));
+
+    if (!isInteger) {
+      interaction.reply({ content: '❌ Please provide a valid group ID.', ephemeral: true });
+      return;
+    }
+
+    groupId = parseInt(groupIdValue);
+  }
+
+  try {
+    await claimLeagueBenefits(interaction.user.id, username, groupId);
+
+    let successMessage = '✅ Your benefits have been claimed!';
+
+    if (groupId) {
+      successMessage += ` You can edit your group's images and social links on your group's edit page on the website.`;
+    }
+
+    interaction.reply({ content: successMessage, ephemeral: true });
+
+    sendModLog(
+      interaction.guild,
+      `${interaction.user} has claimed ${
+        groupId ? 'Tier 2' : 'Tier 1'
+      } (League) Patreon benefits for: \nUsername: ${username}${groupId ? `\nGroup id: ${groupId}` : ''}`
+    );
+  } catch (error) {
     interaction.reply({ content: error.message, ephemeral: true });
   }
 }
