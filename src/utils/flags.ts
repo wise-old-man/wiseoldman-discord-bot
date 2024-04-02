244c3756c20a8e45eadf1715cd68c280ad8db2bc
Psikoi <ruben.amendoeira@gmail.com>
2024-04-02 19:27:19 +0200

    Bump WOM utils to allow GB constituents

--- src/utils/flags.ts
+++ src/utils/flags.ts
@@ -1,24 +1,18 @@
+import { Country } from '@wise-old-man/utils';
+
 // offset between uppercase ascii and regional indicator symbols
 const OFFSET = 127397;
 
-export function countryCodeEmoji(cc: string): string {
-  if (cc === 'USA') return countryCodeEmoji('US');
-  if (cc === 'UK') return countryCodeEmoji('GB');
+export function countryCodeEmoji(cc: Country): string {
+  if (cc === Country.GB_SCT) return '🏴󠁧󠁢󠁳󠁣󠁴󠁿';
+  if (cc === Country.GB_WLS) return '🏴󠁧󠁢󠁷󠁬󠁳󠁿';
+  if (cc === Country.GB_NIR) return '🇬🇧';
 
   if (!/^[a-z]{2}$/i.test(cc)) {
     return '';
   }
 
   const codePoints = [...cc.toUpperCase()].map(c => (c.codePointAt(0) || 0) + OFFSET);
-  return String.fromCodePoint(...codePoints);
-}
-
-export function emojiCountryCode(flag: string): string {
-  if (flag.length !== 4) {
-    return '';
-  }
-
-  const codePoints = [...flag].map(c => (c.codePointAt(0) || 0) - OFFSET);
   return String.fromCodePoint(...codePoints);
 }
 
