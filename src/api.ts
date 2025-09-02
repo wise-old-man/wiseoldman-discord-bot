7ec93e8a454bf07b771f500ce46a1b3f151409fa
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2025-09-02 13:33:08 +0200

    Switch discord-bot api back to blind acks

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,3 @@
-import { isErrored } from '@attio/fetchable';
 import cors from 'cors';
 import { Client } from 'discord.js';
 import express from 'express';
@@ -22,18 +21,7 @@ export function init(client: Client) {
    * These requests contain events that the bot should attempt to propagate to relevant discord servers.
    */
   app.post('/event', async (req, res) => {
-    const result = await onEventReceived(client, req.body);
-
-    if (isErrored(result)) {
-      switch (result.error.code) {
-        case 'EVENT_TYPE_NOT_FOUND':
-          return res.status(400).json(result.error);
-        case 'FAILED_TO_EXECUTE_EVENT':
-          // This shouldn't be propagated to the API. The API has done its job by sending the event to the bot.
-          // If we were to propagate this error, the API would just retry sending the event, which would likely fail again.
-          break;
-      }
-    }
+    onEventReceived(client, req.body);
 
     return res.json('Event received.');
   });
